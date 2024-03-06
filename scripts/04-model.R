615db0ea43cbf83d9c75c9b7c8a0e1c4ae81ff8a
Vanshika <vanshika.vanshika@mail.utoronto.ca>
2024-03-05 20:28:46 -0500

    Final paper

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -8,30 +8,37 @@
 # Any other information needed? [...UPDATE THIS...]
 
 
+
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet("data/analysis_data/analysis_data.parquet")
+
+set.seed(853)
+
+analysis_data <- 
+  ces2020 |> 
+  slice_sample(n = 1000)
+
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
+    formula = voted_for ~ gender + education,
     data = analysis_data,
-    family = gaussian(),
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
   )
 
+prior_summary(first_model)
 
 #### Save model ####
 saveRDS(
   first_model,
   file = "models/first_model.rds"
 )
-
-
